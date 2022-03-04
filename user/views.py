74b03e595b5a2262b9a3415d0e9042b444c3ea34
shubh123 <shubh@example.com>
2022-03-04 16:37:34 +0530

    initial commit

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .models import User
 from .forms import PostForm
+from django.contrib import messages
 
 def addPost(request):
     form=PostForm()
@@ -9,8 +10,10 @@ def addPost(request):
             form=PostForm(request.POST)
             if form.is_valid():
                 form.save()
+                messages.success(request,"Posted successfully")
                 return redirect('addPost')
         else:
+            messages.error(request,"User not authenticated")
             print("User not authenticated")
 
     context={'form':form}
